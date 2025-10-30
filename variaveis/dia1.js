ce59d9da28be0253d28636d1ef385eab1224509f
Edenilson <freitaseddie126@gmail.com>
2025-10-30 19:23:18 -0300

    adicionado tipos de variáveis, listas(Array) e Objeto

--- variaveis/dia1.js
+++ variaveis/dia1.js
@@ -11,3 +11,26 @@ console.log(idade);
 // Declarando uma variável sem var, let ou const (não recomendado)
 cidade = "são Paulo"; // Isso cria uma variável global
 console.log(cidade);
+
+//  Tipos de Dados em Variáveis
+let numero = 10; // Número inteiro
+let numeroDecimal = 10.5; // Número decimal
+let texto = "olá mundo"; // String
+let booleano = true; // Booleano
+let nulo =null; // Nulo
+
+// Lista (Array)
+let list = [1,2,3,4,5];
+console.log(list);
+
+// objeto
+let pessoa = {
+    nome: "Marta",
+    idade: 30,
+    sexo: "Feminino"
+};
+console.log(pessoa.nome);
+
+
+
+
