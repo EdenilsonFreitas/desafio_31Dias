147c0f7cc98c03820b7b78ade9df3ed784b0d6c6
Edenilson <freitaseddie126@gmail.com>
2025-10-30 18:56:43 -0300

    first commit

---
+++ variaveis/index.js
@@ -0,0 +1,13 @@
+// Declarando uma variável com let
+let nome = "João"; // Você pode mudar o valor depois
+console.log(nome);
+nome = "Edenilson";
+console.log(nome);
+// Declarando uma variável com const
+const idade = 25; // O valor não pode ser alterado
+console.log(idade);
+// Tentando alterar o valor de uma constante (isso causarará um erro)
+// idade = 30; // Descomente esta linha para ver o erro
+// Declarando uma variável sem var, let ou const (não recomendado)
+cidade = "são Paulo"; // Isso cria uma variável global
+console.log(cidade);
